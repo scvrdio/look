fd7c4b1c949112b6f38ca74d9e931b9c39118267
serezha <scvrdio@gmail.com>
2026-02-10 21:55:52 +0300

    minor changes

--- src/components/series/EpisodeButton.tsx
+++ src/components/series/EpisodeButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { hapticSelection } from "@/lib/haptics";
 
 type EpisodeButtonProps = {
   number: number;
@@ -12,15 +13,16 @@ export function EpisodeButton({ number, watched, onClick }: EpisodeButtonProps) 
   return (
     <button
       type="button"
-      onClick={onClick}
+      onClick={() => {
+        hapticSelection();
+        onClick();
+      }}
       className={cn(
         "h-16 w-16 rounded-full",
         "flex items-center justify-center",
         "text-[16px] font-medium",
         "transition active:scale-[0.95]",
-        watched
-          ? "bg-black text-white"
-          : "bg-black/4 text-black"
+        watched ? "bg-black text-white" : "bg-black/4 text-black"
       )}
     >
       {number}
